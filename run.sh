57dfc496b12e72f401fa54ad976ccf2d18e8dc63
nicoIwas <nickuhr@usp.br>
2023-11-15 17:12:15 -0300

    atualizando o script que vai rodar tudo aqui direto

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-python3 brasil.py
-python3 brasilivre.py
+python3 brasil.py > output1.txt
+python3 brasilivre.py > output2.txt
+python3 brasilivre2.py > output3.txt
+python3 brasilivre_final.py > output4.txt
